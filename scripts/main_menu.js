358c09c8c2e9b4fbd13db49dde9ae79374c793f1
braisque <braisque@yahoo.fr>
2026-09-06 18:57:32 +0200

    Replace menu icon pngs with svgs

--- scripts/main_menu.js
+++ scripts/main_menu.js
@@ -4,15 +4,25 @@
 
     const menuHTML = `
     <nav id="bar">
+        <a href="${root}index.html" title="Retourner à l'accueil" target="_parent">
+            <img src="${root}images/icons/home.svg" alt="" />
+            <span>Accueil</span>
+        </a>
 
-        <a href="${root}index.html" title="Retourner à l'accueil" target="_parent"><img src="${root}images/icons/home.png" alt="accueil" /></a>
+        <a href="${root}misc/ebooks.html" title="Télécharger les écrits formatés pour la lecture hors-ligne ou sur tablette" target="_parent">
+            <img src="${root}images/icons/ebook.svg" alt="" />
+            <span>E-books</span>
+        </a>
 
-	    <a href="${root}misc/ebooks.html" title="Télécharger les écrits formatés pour la lecture hors-ligne ou sur tablette" target="_parent"><img src="${root}images/icons/ebook.png" alt="ebook" /></a>
+        <a href="${root}misc/language.html" title="For those who do not speak French" target="_parent">
+            <img src="${root}images/icons/language.svg" alt="" />
+            <span>Language</span>
+        </a>
 
-	    <a href="${root}misc/language.html" title="For those who do not speak French" target="_parent"><img src="${root}images/icons/language.png" alt="language" /></a>
-
-	    <a href="${root}misc/contacts.html" title="Liens et contacts" target="_parent"><img src="${root}images/icons/contact.png" alt="contact" /></a>
-        
+        <a href="${root}misc/contacts.html" title="Liens et contacts" target="_parent">
+            <img src="${root}images/icons/contact.svg" alt="" />
+            <span>Contact</span>
+        </a>
     </nav>
 
     <hr>
