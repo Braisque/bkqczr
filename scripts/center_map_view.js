bfb5bf3b478b62611a0ebc95678e8b2acb381dda
braisque <braisque@yahoo.fr>
2026-09-06 21:36:50 +0200

    Fix map and menu centering

---
+++ scripts/center_map_view.js
@@ -0,0 +1,5 @@
+window.onload = () =>
+{
+    const scrollX = (4096 - window.innerWidth) / 2;
+    window.scrollTo(scrollX, 0);
+};
--- scripts/center_view.js
+++
@@ -1,3 +0,0 @@
-const header = document.querySelector('h1');
-header.scrollIntoView({ block: 'start', inline: 'center' });
-window.scrollTo(window.scrollX, 0);
